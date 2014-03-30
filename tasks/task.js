edd253f07ea420b6c28e031bf2e6810c6523173f
Parashuram <code@r.nparashuram.com>
2014-03-30 11:10:02 -0700

    Minor Fixes
    Remove console.log to prevent printing the URI on error so that password is not exposed
    Bumped package version
    Corrected error message

--- tasks/task.js
+++ tasks/task.js
@@ -10,20 +10,22 @@ module.exports = function(grunt) {
           info: grunt.log.ok.bind(grunt.log),
           debug: grunt.verbose.writeln.bind(grunt.verbose),
           trace: grunt.log.debug.bind(grunt.log)
-        }
+        },
+        time: new Date().getTime()
       }),
       files = options.urls;
 
+    options.time = parseFloat(options.time, 10);
 
     if (options.sauceTunnel) {
       var SauceTunnel = require('sauce-tunnel');
-      grunt.verbose.writeln('Starting Saucelabs Tunnel');
-      var tunnel = new SauceTunnel(options.SAUCE_USERNAME, options.SAUCE_ACCESS_KEY, 'tunnel', true);
+      grunt.log.writeln('Starting Saucelabs Tunnel');
+      var tunnel = new SauceTunnel(options.SAUCE_USERNAME, options.SAUCE_ACCESSKEY, options.sauceTunnel, true);
       tunnel.start(function(status) {
         if (status === false) {
           grunt.log.fail('Could not open tunnel to saucelabs to testing');
         }
-        grunt.verbose.writeln('Saucelabs Tunnel started');
+        grunt.log.ok('Saucelabs Tunnel started');
         runPerfTest(files, options, function(res) {
           grunt.verbose.writeln('All perf tests completed');
           tunnel.stop(function() {
@@ -51,12 +53,12 @@ module.exports = function(grunt) {
               success = false;
               grunt.log.warn(err);
             } else {
-              grunt.log.ok(res);
+              grunt.log.ok('Saved performance metrics');
             }
             runTest(i + 1);
           }
         };
-        ['suite', 'browsers', 'selenium', 'SAUCE_ACCESS_KEY', 'SAUCE_USERNAME', 'time', 'run', 'couchdb'].forEach(function(prop) {
+        ['suite', 'browsers', 'selenium', 'SAUCE_ACCESSKEY', 'SAUCE_USERNAME', 'time', 'run', 'couch'].forEach(function(prop) {
           config[prop] = options[prop]
         });
         perfjankie(config);

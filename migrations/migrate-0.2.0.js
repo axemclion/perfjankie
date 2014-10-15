30a94c6fd52d6f22a5466de57e71b4961dab55b2
Parashuram <code@r.nparashuram.com>
2014-10-14 19:24:04 -0700

    Convert NavTimings to absolute

--- migrations/migrate-0.2.0.js
+++ migrations/migrate-0.2.0.js
@@ -13,16 +13,45 @@ module.exports = function(oldDb, newDb, config) {
 		}
 		delete doc._id;
 		delete doc._rev;
-		var metrics = {};
-
-		for (var key in doc.data) {
-			var d = doc.data[key];
-			doc.data[key] = d.value;
-		}
 		doc.url = null;
 		doc.browser = doc.meta._browserName || null;
 
-		// Reset Nav timings from absolute
+		for (var key in doc.data) {
+			doc.data[key] = doc.data[key].value;
+		}
+
+
+		doc.data[events[0]] = 0;
+		for (var i = 1; i < events.length; i++) {
+			if (typeof doc.data[events[i]] === 'undefined') {
+				doc.data[events[i]] = 0;
+			}
+			doc.data[events[i]] = doc.data[events[i]] + doc.data[events[i - 1]];
+		}
+
 		return doc;
 	});
 };
+
+var events = [
+	'navigationStart',
+	'unloadEventStart',
+	'unloadEventEnd',
+	'redirectStart',
+	'redirectEnd',
+	'fetchStart',
+	'domainLookupStart',
+	'domainLookupEnd',
+	'connectStart',
+	'connectEnd',
+	'secureConnectionStart',
+	'requestStart',
+	'responseStart',
+	'domLoading',
+	'domInteractive',
+	'domContentLoadedEventStart',
+	'domContentLoadedEventEnd',
+	'domComplete',
+	'loadEventStart',
+	'loadEventEnd'
+];

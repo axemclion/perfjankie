bd3db1d814ac868b926e5f8e7a1298f7a7c9e7a5
Parashuram <code@r.nparashuram.com>
2014-11-02 10:29:11 -0800

    Fix JSHint errors

--- www/server/endpoints.js
+++ www/server/endpoints.js
@@ -17,7 +17,7 @@ angular
 
 			var server = {
 				'/pagelist': function() {
-					return $http.get(DB_BASE + '/pagelist/_view/pages?group=true').then(function(resp) {
+					return $http.get(window.DB_BASE + '/pagelist/_view/pages?group=true').then(function(resp) {
 						var result = {};
 						angular.forEach(resp.data.rows, function(row) {
 							var res = rowsToObj(row, ['suite', 'pagename', 'browser'], 'runCount');
@@ -35,7 +35,7 @@ angular
 					return $q.when(window.METRICS_LIST);
 				},
 				'/runList': function(opts) {
-					return $http.get(DB_BASE + '/runs/_view/list', {
+					return $http.get(window.DB_BASE + '/runs/_view/list', {
 						params: {
 							endkey: JSON.stringify([opts.browser, opts.pagename, null]),
 							startkey: JSON.stringify([opts.browser, opts.pagename, {}]),
@@ -52,7 +52,7 @@ angular
 				},
 				'/runData': function(opts) {
 					opts.time = parseInt(opts.time, 10);
-					return $http.get(DB_BASE + '/runs/_view/data', {
+					return $http.get(window.DB_BASE + '/runs/_view/data', {
 						params: {
 							startkey: JSON.stringify([opts.browser, opts.pagename, opts.time, null]),
 							endkey: JSON.stringify([opts.browser, opts.pagename, opts.time, {}]),
@@ -79,7 +79,7 @@ angular
 					if (!isNaN(limit)) {
 						config.params.limit = limit;
 					}
-					return $http.get(DB_BASE + '/metrics_data/_view/stats', config).then(function(resp) {
+					return $http.get(window.DB_BASE + '/metrics_data/_view/stats', config).then(function(resp) {
 						var res = [];
 						angular.forEach(resp.data.rows, function(obj, index) {
 							obj.label = obj.key[4];

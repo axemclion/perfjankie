45117208065f2ce8dfb1003b175cad208ce62a40
Parashuram <code@nparashuram.com>
2015-06-10 10:30:00 -0700

    Changed sidebar and page select

--- www/app/main-page/sidebar.js
+++ www/app/main-page/sidebar.js
@@ -1,12 +1,20 @@
 angular
 	.module('sidebar', ['ngRoute'])
-	.controller('SideBarCtrl', [
-
-		function() {
-			this.categories = {
-				js: ['EvaluateScript', 'TimerFire', 'EventDispatch', 'FunctionCall'],
-				paint: ['Paint', 'Layout', 'RecalculateStyles', 'CompositeLayers'],
-				content: ['ParseHTML', 'DecodeImage']
-			};
+	.controller('SideBarCtrl', ['$routeParams', '$scope',
+		function($routeParams, $scope) {
+			var self = this;
+			$scope.$on('$routeChangeSuccess', function(scope, next, current) {
+				var browser = $routeParams.browser;
+				self.categories = {
+					'Frame Rates': ['framesPerSec_raf', 'meanFrameTime', 'droppedFrameCount'],
+				};
+				if (['chrome', 'safari', 'android'].indexOf(browser) !== -1) {
+					self.categories['Paint'] = ['Paint', 'Layout', 'RecalculateStyles', 'CompositeLayers'];
+					self.categories['Javascript'] = ['TimerInstall', 'TimerFire', 'EventDispatch', 'FunctionCall'];
+				}
+				if (browser !== 'safari'){
+					self.categories['Network'] = ['domInteractive', 'domLoading'];
+				}
+			});
 		}
 	]);

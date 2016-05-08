ea51088d1ba8f09ba55a9874d918c1dbb983d93b
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-05-08 15:16:21 +0200

    Travis CI build

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,68 +1,68 @@
 var expect = require('chai').expect,
-	sinon = require('sinon'),
-	fs = require('fs'),
-	nano = require('nano');
+    sinon = require('sinon'),
+    fs = require('fs'),
+    nano = require('nano');
 
 require('q').longStackSupport = true;
 
-describe('App', function() {
-	var browserPerfStub = sinon.stub();
-	var sampleData;
-	var util = require('./util'),
-		app = require('../'),
-		config = util.config({});
+describe('App', function () {
+    var browserPerfStub = sinon.stub();
+    var sampleData;
+    var util = require('./util'),
+        app = require('../'),
+        config = util.config({});
 
-	before(function(done) {
-		var server = nano(config.couch.server);
-		server.db.destroy(config.couch.database, function(err, res) {
-			done();
-		});
-	});
+    before(function (done) {
+        nano(config.couch.server).db.destroy(config.couch.database, function(err, res) {
+            done();
+        });
+    });
 
-	beforeEach(function() {
-		config.log.info('===========');
-		sampleData = JSON.parse(fs.readFileSync(__dirname + '/res/sample-perf-results.json', 'utf8'));
-		browserPerfStub.callsArgWith(1, null, sampleData);
-	});
+    beforeEach(function () {
+        config.log.info('===========');
+        sampleData = JSON.parse(fs.readFileSync(__dirname + '/res/sample-perf-results.json', 'utf8'));
+        browserPerfStub.callsArgWith(1, null, sampleData);
+    });
 
-	it('should only update data', function(done) {
-		app(util.config({
-			couch: {
-				updateSite: false
-			},
-			callback: function(err, res) {
-				expect(err).to.not.be.ok;
-				expect(res).to.be.ok;
-				done();
-			},
-			browserPerf: browserPerfStub
-		}));
-	});
+    it('should only update data', function (done) {
+        app(util.config({
+            couch: {
+                updateSite: false
+            },
+            callback: function (err, res) {
+                console.log(err);
+                expect(err).to.not.be.ok;
+                expect(res).to.be.ok;
+                done();
+            },
+            browserPerf: browserPerfStub
+        }));
+    });
 
-	it('should only update site', function(done) {
-		var couchDataStub = sinon.stub();
-		couchDataStub.callsArgWith(2, null, []);
-		app(util.config({
-			couch: {
-				onlyUpdateSite: true
-			},
-			callback: function(err, res) {
-				expect(couchDataStub.called).to.not.be.true;
-				expect(err).to.not.be.ok;
-				expect(res).to.be.ok;
-				done();
-			}
-		}));
-	});
+    it('should only update site', function (done) {
+        var couchDataStub = sinon.stub();
+        couchDataStub.callsArgWith(2, null, []);
+        app(util.config({
+            couch: {
+                onlyUpdateSite: true
+            },
+            callback: function (err, res) {
+                expect(couchDataStub.called).to.not.be.true;
+                expect(err).to.not.be.ok;
+                expect(res).to.be.ok;
+                done();
+            }
+        }));
+    });
 
-	it('should run performance tests and save results in a database', function(done) {
-		app(util.config({
-			callback: function(err, res) {
-				expect(err).to.not.be.ok;
-				expect(res).to.be.ok;
-				done();
-			},
-			browserPerf: browserPerfStub
-		}));
-	});
+    it('should run performance tests and save results in a database', function (done) {
+        app(util.config({
+            callback: function (err, res) {
+                expect(err).to.not.be.ok;
+                expect(res).to.be.ok;
+                done();
+            },
+            browserPerf: browserPerfStub
+        }));
+    });
 });
